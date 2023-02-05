a858a26173284480c90792f1a601e3be293ea475
Eliza Upadhyaya <eliza.upadhyaya@gmail.com>
2023-02-05 15:23:13 +0000

    completed all the basic CRUD actions for Tasks Manager App

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "tasks#index"
+  # read all
+  root 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # create new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # show 1 task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy '
 end
